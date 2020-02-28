b2448cafb211a71749f862a880742783e38d1e60
bwb63 <bwb63@txstate.edu>
2020-02-28 02:26:37 -0600

    Test site for sxsw

---
+++ startbootstrap-landing-page-gh-pages/popupForm/handler.php
@@ -0,0 +1,33 @@
+<?php
+  header('Access-Control-Allow-Origin: *'); 
+  header("Access-Control-Allow-Credentials: true");
+  header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+  header('Access-Control-Max-Age: 1000');
+  header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+Tested working with PHP5.4 and above (including PHP 7 )
+
+ */
+require_once './vendor/autoload.php';
+
+use FormGuide\Handlx\FormHandler;
+
+
+$pp = new FormHandler(); 
+
+$validator = $pp->getValidator();
+$validator->fields(['name','email'])->areRequired()->maxLength(50);
+$validator->field('email')->isEmail();
+$validator->field('message')->maxLength(6000);
+
+
+
+
+$pp->sendEmailTo('benbond96@gmail.com'); // ← Your email here
+
+echo $pp->process($_POST);
